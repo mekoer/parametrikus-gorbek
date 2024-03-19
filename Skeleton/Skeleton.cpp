6d12a6b5e698e52ff13af77007c9c78e44ad4c66
mayad <mayadam77@gmail.com>
2024-03-19 18:15:04 +0100

    alap fuggvenyek, osztalyok

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : 
-// Neptun : 
+// Nev    : Mayer Ádám
+// Neptun : XYJP9S
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
@@ -33,16 +33,18 @@
 //=============================================================================================
 #include "framework.h"
 
+using namespace std;
+
 // vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
 const char * const vertexSource = R"(
 	#version 330				// Shader 3.3
 	precision highp float;		// normal floats, makes no difference on desktop computers
 
 	uniform mat4 MVP;			// uniform variable, the Model-View-Projection transformation matrix
-	layout(location = 0) in vec2 vp;	// Varying input: vp = vertex position is expected in attrib array 0
+	layout(location = 0) in vec3 cp;	// Varying input: vp = control point is expected in attrib array 0
 
 	void main() {
-		gl_Position = vec4(vp.x, vp.y, 0, 1) * MVP;		// transform vp from modeling space to normalized device space
+		gl_Position = vec4(cp.x, cp.y, cp.z, 1) * MVP;		// transform vp from modeling space to normalized device space
 	}
 )";
 
@@ -60,29 +62,151 @@ const char * const fragmentSource = R"(
 )";
 
 GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+
+enum State {
+	IDLE,
+	LAGRANGE,
+	BEZIER,
+	CATMULLROM
+};
+
+State mode = IDLE;
+
+class Camera {
+	vec3 cam;
+	float size = 30;
+	mat4 viewM, projM;
+public:
+	Camera() {
+		cam = vec3(0, 0, 0);
+	}
+
+	void V() {
+		float ratio = size / 30;
+		projM = { ratio, 0, 0, 0,
+				  0, ratio, 0, 0,
+				  0, 0, ratio, 0,
+				  cam.x, cam.y, cam.z, 1 };
+	}
+
+	void P() {
+		projM = { 1, 0, 0, 0,
+				  0, 1, 0, 0,
+				  0, 0, 1, 0,
+				  0, 0, 0, 1 };
+	}
+
+	void pan(int panvalue) {
+		cam.y += panvalue;
+		V();
+	}
+
+	void zoom(int zoomvalue) {
+		size *= zoomvalue;
+		V();
+	}
+};
+
+class Curve {
+	unsigned int vao, vbo[2]; // vbo[0] -> cps, vbo[1] -> gorbe
+protected:
+	vector<vec3> cps;
+public:
+	Curve() {
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		glGenBuffers(2, vbo);
+
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	}
+
+	void updateGPU(int vboid) {
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[vboid]);
+		glBufferData(GL_ARRAY_BUFFER, cps.size() * sizeof(vec3), &cps[0], GL_DYNAMIC_DRAW);
+	}
+
+	void Draw(int type, vec3 color) {
+		if (cps.size() > 0) {
+			glBindVertexArray(vao);
+			gpuProgram.setUniform(color, "color");
+			glDrawArrays(type, 0, cps.size());
+		}
+	}
+};
+
+class Lagrange : Curve {
+	vector<float> ts; // knots
+
+	float L(int i, float t) {
+		float Li = 1.0f;
+		for (int j = 0; j < cps.size(); j++)
+			if (j != i) {
+				Li = Li * ((t - ts[j]) / (ts[i] - ts[j]));
+			}
+		return Li;
+	}
+public:
+	Lagrange() : Curve() {}
+
+	void AddControlPoint(vec3 cp) {
+		float ti = cps.size(); // or something better
+		cps.push_back(cp);
+		ts.push_back(ti);
+	}
+
+	vec3 r(float t) {
+		vec3 rt(0, 0, 0);
+		for (int i = 0; i < cps.size(); i++) {
+			rt = rt + cps[i] * L(i, t);
+		}
+		return rt;
+	}
+
+};
+
+class Bezier : Curve {
+	vector<vec3> cps; // control pts
+	float B(int i, float t) {
+		int n = cps.size() - 1; // n+1 pts!
+		float choose = 1;
+		for (int j = 1; j <= i; j++) choose *= (float)(n - j + 1) / j;
+		return choose * pow(t, i) * pow(1 - t, n - i);
+	}
+public:
+	void AddControlPoint(vec3 cp) {
+		cps.push_back(cp);
+	}
+	vec3 r(float t) {
+		vec3 rt(0, 0, 0);
+		for (int i = 0; i < cps.size(); i++) {
+			rt = rt + (cps[i] * B(i, t));
+		}
+		return rt;
+	}
+
+};
+
+class CatmullRom : Curve {
+
+};
+
+Lagrange* lagrange;
+
+
 
 // Initialization, create an OpenGL context
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
 
-	glGenVertexArrays(1, &vao);	// get 1 vao id
-	glBindVertexArray(vao);		// make it active
-
-	unsigned int vbo;		// vertex buffer object
-	glGenBuffers(1, &vbo);	// Generate 1 buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-	float vertices[] = { -0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f };
-	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-		sizeof(vertices),  // # bytes
-		vertices,	      	// address
-		GL_STATIC_DRAW);	// we do not change later
-
-	glEnableVertexAttribArray(0);  // AttribArray 0
-	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-		0, NULL); 		     // stride, offset: tightly packed
+	lagrange = new Lagrange;
 
 	// create program for the GPU
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
@@ -105,15 +229,25 @@ void onDisplay() {
 	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
 	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
 
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
+	//lagrange->Draw();
 
 	glutSwapBuffers(); // exchange buffers for double buffering
 }
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+	switch (key)
+	{
+	case 'l':
+		mode = LAGRANGE;
+		break;
+	case 'b':
+		mode = BEZIER;
+		break;
+	case 'c':
+		mode = CATMULLROM;
+		break;
+	}
 }
 
 // Key of ASCII code released
@@ -134,17 +268,26 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
+	if (state == GLUT_DOWN) {
+		switch (mode)
+		{
+		case IDLE:
+			break;
+		case LAGRANGE:
+
+			break;
+		case BEZIER:
+			break;
+		case CATMULLROM:
+			break;
+		}
 	}
 
-	switch (button) {
+	/*switch (button) {
 	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
 	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
 	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
+	}*/
 }
 
 // Idle event indicating that some time elapsed: do animation here
